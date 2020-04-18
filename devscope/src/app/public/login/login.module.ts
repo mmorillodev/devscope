63b45bb91564eed1277edfee750e8186f16f4207
mmorillo <matheus.morillo2000@gmail.com>
2020-04-18 17:30:24 -0300

    Created component for login

--- devscope/src/app/public/login/login.module.ts
+++ devscope/src/app/public/login/login.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
         LoginComponent
     ],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
     exports: [ReactiveFormsModule],
     bootstrap: [LoginComponent]
 })
