6ca4df8845b3b128d2e3cab8b49b42f79cd7bc0d
ITERIS\leonardo.mesquita <leonardo.mesquita@iteris.com.br>
2020-04-26 22:22:23 -0300

    Ajustando o layout da página de cadastro

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
     registerFirstForm: FormGroup;
+    registerSecondForm: FormGroup;
     secondFormOptions = [];
     firstFormOptions = [];
 
@@ -17,15 +18,26 @@ export class RegisterComponent implements OnInit {
     ngOnInit() {
         this.registerFirstForm = this.formBuilder.group({
             fullName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
-            emailGroup: this.formBuilder.group({
-                email: ['', [Validators.required, Validators.email]],
-                confirmEmail: ['', [Validators.required]]
-            }),
-            password: ['', [Validators.required]],
+            genre: ['', Validators.required],
             birthDate: ['', [Validators.required]],
             telephone: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
             cellphone: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-            address: ['', [Validators.required]]
+            address: ['', [Validators.required]],
+            addressNumber: ['', Validators.required],
+            state: ['', Validators.required],
+            city: ['', Validators.required],
+            cep: ['', Validators.required]
+        });
+
+        this.registerSecondForm = this.formBuilder.group({
+            emailGroup: this.formBuilder.group({
+                email: ['', [Validators.required, Validators.email]],
+                confirmEmail: ['', [Validators.required]]
+            }, { validator: this.emailMatcher }),
+            passwordGroup: this.formBuilder.group({
+                password: ['', [Validators.required]],
+                confirmPassword: ['', [Validators.required]]
+            }, { validator: this.passwordMatcher})
         });
 
         this.firstFormOptions = [
@@ -43,6 +55,49 @@ export class RegisterComponent implements OnInit {
         ];
     }
 
+    mustMatch(formControl: AbstractControl, controlName: string, matchControlName: string): { [key: string]: boolean } | null {
+        const control = formControl.get(controlName);
+        const matchControl = formControl.get(matchControlName);
+
+        if (control.pristine || matchControl.pristine) {
+            return null;
+        }
+
+        if (control.value === matchControl.value) {
+            return null;
+        }
+
+        return { 'match': true };
+    }
+
+    emailMatcher(formControl: AbstractControl, teste: string): { [key: string]: boolean } | null {
+        const emailControl = formControl.get('email');
+        const confirmEmailControl = formControl.get('confirmEmail');
+
+        if (emailControl.pristine || confirmEmailControl.pristine) {
+          return null;
+        }
+
+        if (emailControl.value === confirmEmailControl.value) {
+          return null;
+        }
+        return { 'match': true };
+    }
+
+    passwordMatcher(formControl: AbstractControl): { [key: string]: boolean } | null {
+        const passwordControl = formControl.get('password');
+        const confirmPasswordControl = formControl.get('confirmPassword');
+
+        if (passwordControl.pristine || confirmPasswordControl.pristine) {
+          return null;
+        }
+
+        if (passwordControl.value === confirmPasswordControl.value) {
+          return null;
+        }
+        return { 'match': true };
+    }
+
     firstFormRegister() {
         this.firstFormOptions[0].disabled = true;
         this.firstFormOptions[0].expanded = false;
@@ -51,6 +106,14 @@ export class RegisterComponent implements OnInit {
     }
 
     secondFormRegister() {
+        // console.log('Dirty:', this.registerFirstForm.dirty);
+        // console.log('Touched:', this.registerFirstForm.touched);
+        // console.log('Valid:', this.registerFirstForm.valid);
+        // console.log('Value', this.registerFirstForm.value);
 
+        // console.log('Dirty:', this.registerSecondForm.dirty);
+        // console.log('Touched:', this.registerSecondForm.touched);
+        // console.log('Valid:', this.registerSecondForm.valid);
+        // console.log('Value', this.registerSecondForm.value);
     }
 }
