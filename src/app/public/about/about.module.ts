30f24accd2e1ba215006a57bad471e0b0ffb6cde
mmorillo <matheus.morillo2000@gmail.com>
2020-04-26 21:43:55 -0300

    added router action from toolbar to about page

--- src/app/public/about/about.module.ts
+++ src/app/public/about/about.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { AboutComponent } from './about.component';
 import {BongoCatModule} from '../../shared/components/bongo-cat/bongo-cat.module';
 import {AboutFloatingCardModule} from '../../shared/components/about-floating-card/about-floating-card.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AboutComponent
   ],
-  imports: [BongoCatModule, AboutFloatingCardModule],
+  imports: [BongoCatModule, AboutFloatingCardModule, RouterModule],
   exports: [],
   bootstrap: [AboutComponent]
 })
